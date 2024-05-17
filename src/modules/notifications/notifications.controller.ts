1ece0a223a8cd31c75203ecf7bea14dcc6f7435e
kambangSinclaire <kambangsinclaire@gmail.com>
2024-05-17 06:18:52 +0100

    fix email notifications

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,10 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Sse, Post, Body, Res, MessageEvent, Param } from '@nestjs/common';
-import { Observable, interval, of } from 'rxjs';
+import { Controller, Sse, Post, Body, MessageEvent, Param } from '@nestjs/common';
+import { Observable, interval } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { INotification } from 'src/models/interfaces/notification.interface';
 import { NotificationsService } from './notifications.service';
-import { Response } from 'express';
 import { EventPattern } from '@nestjs/microservices';
 import { MICROSERVICE_EVENTS } from 'src/common/constants/microservice.constants';
 import { CacheManager } from 'src/cache/redis/cache.utils';
@@ -15,15 +13,13 @@ export class NotificationsController {
     constructor(private notificationsService: NotificationsService) { }
 
     @EventPattern(MICROSERVICE_EVENTS.SEND_USER_EMAIL)
-    async sendUserEmail(@Body() payload: any, @Res() res: Response) {
-        const response =  await this.notificationsService.sendEmail(payload);
-        res.status(200).send(response)
+    async sendUserEmail(@Body() payload: any) {
+        await this.notificationsService.sendEmail(payload);
     }
 
     @EventPattern(MICROSERVICE_EVENTS.PRODUCT_THRESHOLD_REACHED)
-    async sendEmailToOrgAdmin(@Body() payload: any, @Res() res: Response) {
-        const response =  await this.notificationsService.sendEmail(payload);
-        res.status(200).send(response)
+    async sendEmailToOrgAdmin(@Body() payload: any) {
+        await this.notificationsService.sendEmail(payload);
     }
 
     @Post("push/send")
